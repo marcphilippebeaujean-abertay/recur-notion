074c0c115e7b6c149d80fc9b4f5b070894340eef
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2021-12-25 07:32:09 +0100

    executing add django admin in the build stage via CLI

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,5 @@ pip install notion-client
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('${DJANGO_ADMIN_USERNAME}', '${DJANGO_ADMIN_EMAIL}', '${DJANGO_ADMIN_PASSWORD}')" | python manage.py shell
--- tasks/jobs.py
+++ tasks/jobs.py
@@ -1,5 +1,4 @@
 import notion_client
-from notion_client import APIResponseError
 
 from accounts.models import CustomUser
 
@@ -13,14 +12,14 @@ def create_notion_tasks_from_recurring_user_tasks():
     users = CustomUser.objects.filter(workspace_access__isnull=False,
                                       workspace_access__access_token__isnull=False)
     for user in users:
-        tasks_to_create = [task for task in list(user.tasks.all()) if task.should_create_task_today]
+        tasks_to_create_list = [task for task in list(user.tasks.all()) if task.should_create_task_today]
         workspace_access_queryset = user.workspace_access.all()
         if workspace_access_queryset.count is 0:
             continue
-        for task in tasks_to_create:
+        for task_model in tasks_to_create_list:
             client = notion_client.Client(auth=list(workspace_access_queryset)[0].access_token)
-            notion_page_resp_dict = client.pages.retrieve(task.cloned_task_notion_id)
+            notion_page_resp_dict = client.pages.retrieve(task_model.cloned_task_notion_id)
             page_properties_dict = notion_page_resp_dict['properties']
-            parent_database_dict = {'database_id': task.database_id}
+            parent_database_dict = {'database_id': task_model.database_id}
             client.pages.create(properties=page_properties_dict, parent=parent_database_dict)
-            logger.debug(f'Created recurring task with id{task.cloned_task_notion_id}')
+            logger.debug(f'Created recurring task with id{task_model.cloned_task_notion_id}')
