d2e421e0a03110fca556b99631942a1988bc691c
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2021-12-25 13:45:22 +0100

    fixed bug where not all database items were being shown correctly

--- tasks/service.py
+++ tasks/service.py
@@ -3,6 +3,7 @@ from notion_client import Client
 from workspaces.models import NotionWorkspaceAccess
 
 import logging
+
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
@@ -13,7 +14,6 @@ class NotionApiException(Exception):
 
 def fetch_notion_workspace_pages_and_convert_to_task_dict(user_model, query_string):
     logger.info(f'Fetching workspace pages')
-    logger.info(f'Getting Workspace Access')
     notion_workspace_access_grant_model = NotionWorkspaceAccess.objects.filter(owner=user_model).first()
     if notion_workspace_access_grant_model is None:
         raise NotionApiException(f'User {user_model.username} does not have any access grants!')
@@ -25,9 +25,8 @@ def fetch_notion_workspace_pages_and_convert_to_task_dict(user_model, query_stri
         request_filter_dict['query'] = query_string
     api_response_object_dict_list = client.search(**request_filter_dict).get("results")
 
-    is_api_dict_page = lambda page_dict: 'properties' in page_dict and 'Name' in page_dict['properties']
     api_resp_non_pages_filtered = [api_object_dict for api_object_dict in api_response_object_dict_list
-                                   if is_api_dict_page(api_object_dict)]
+                                   if api_object_dict['object'] == 'page']
 
     is_page_in_database = lambda page_dict: 'parent' in page_dict and page_dict['parent']['type'] == 'database_id'
     pages_in_database_list = [task for task in api_resp_non_pages_filtered if is_page_in_database(task)]
@@ -40,11 +39,22 @@ def fetch_notion_workspace_pages_and_convert_to_task_dict(user_model, query_stri
         recurring_task_list.append(recurring_task)
         recurring_task_by_notion_task_id_dict[recurring_task.cloned_task_notion_id] = recurring_task_list
 
-    convert_page_to_task_dict = lambda page_dict: {
-        'title': page_dict['properties']['Name']['title'][0]['plain_text'],
+    return [convert_api_page_response_dict_to_task_dict(page_dict=page_dict,
+                                                        recurring_task_by_notion_task_id_dict=recurring_task_by_notion_task_id_dict)
+                                                        for page_dict in pages_in_database_list][:10]
+
+
+def convert_api_page_response_dict_to_task_dict(page_dict, recurring_task_by_notion_task_id_dict):
+    task_title_string = 'Invalid Name'
+    properties_dict = page_dict['properties']
+    for key in properties_dict:
+        property_info_dict = properties_dict[key]
+        if 'title' in property_info_dict['type']:
+            task_title_string = property_info_dict['title'][0]['plain_text']
+    return {
+        'title': task_title_string,
         'id': page_dict['id'],
         'url': page_dict['url'],
         'db_id': page_dict['parent']['database_id'],
         'recurring_tasks': recurring_task_by_notion_task_id_dict.get(page_dict['id'], [])
     }
-    return [convert_page_to_task_dict(task) for task in pages_in_database_list][:10]
