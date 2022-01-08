c0f00317fdf819cf01a34e121d6a14c072f5e4f0
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2022-01-08 10:56:58 +0100

    fixed url property fields only accepting literal urls

--- notion_database/service.py
+++ notion_database/service.py
@@ -17,7 +17,7 @@ IGNORED_PROPERTIES_SET = set.union({
     'relation', 'formula', "rollup", "created_time", "created_by", "last_edited_time",
     "last_edited_by", "people", "files"
 }, NOTION_DATE_PROPERTIES_SET)
-NOTION_TEXT_PROPERTIES_SET = {'email', 'phone_number', 'rich_text', 'title'}
+NOTION_TEXT_PROPERTIES_SET = {'email', 'phone_number', 'rich_text', 'title', 'url'}
 NOTION_SELECT_PROPERTIES = {'multi_select', 'select'}
 EMPTY_OPTION_DICT = {'id': '', 'name': ''}
 
