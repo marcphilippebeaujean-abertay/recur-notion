f323fa9e08797f135bac46e95eba20e71dcfab7d
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2022-01-13 13:42:26 +0100

    improved landing page and data policy

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View
 
-# Create your views here.
+
+class UserDeleteView(LoginRequiredMixin, View):
+    """
+    Deletes the currently signed-in user and all associated data.
+    """
+    def get(self, request, *args, **kwargs):
+        form = UserDeleteForm()
+        return render(request, 'users/user_deletion.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = UserDeleteForm(request.POST)
+        # Form will be valid if checkbox is checked.
+        if form.is_valid():
+            user = request.user
+            # Logout before we delete. This will make request.user
+            # unavailable (or actually, it points to AnonymousUser).
+            logout(request)
+            # Delete user (and any associated ForeignKeys, according to
+            # on_delete parameters).
+            user.delete()
+            messages.success(request, 'Account successfully deleted')
+            return redirect(reverse('home'))
+        return render(request, 'users/user_deletion.html', {'form': form})
--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,6 @@ from .views import HomePageView, DataPolicy, Imprint
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
-    path('about/', DataPolicy.as_view(), name='datapolicy'),
-    path('about/', Imprint.as_view(), name='imprint'),
+    path('data-policy/', DataPolicy.as_view(), name='data-policy'),
+    path('imprint/', Imprint.as_view(), name='imprint'),
 ]
