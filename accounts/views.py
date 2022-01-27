6315c5e74824dd21d33878c78645bf0d470b5b11
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2022-01-27 07:38:56 +0100

    added oauth handling for google login and when user is signing up with the same email twice from their social account

--- accounts/views.py
+++ accounts/views.py
@@ -1,28 +1,25 @@
+from allauth.account.utils import perform_login
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
-from django.views.generic import View
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+
+from .app_settings import EmailVerificationMethod
 
 
-class UserDeleteView(LoginRequiredMixin, View):
-    """
-    Deletes the currently signed-in user and all associated data.
-    """
+class SocialAdapter(DefaultSocialAccountAdapter):
+    def is_auto_signup_allowed(self, request, sociallogin):
+        return True
 
-    def get(self, request, *args, **kwargs):
-        form = UserDeleteForm()
-        return render(request, "users/user_deletion.html", {"form": form})
-
-    def post(self, request, *args, **kwargs):
-        form = UserDeleteForm(request.POST)
-        # Form will be valid if checkbox is checked.
-        if form.is_valid():
-            user = request.user
-            # Logout before we delete. This will make request.user
-            # unavailable (or actually, it points to AnonymousUser).
-            logout(request)
-            # Delete user (and any associated ForeignKeys, according to
-            # on_delete parameters).
-            user.delete()
-            messages.success(request, "Account successfully deleted")
-            return redirect(reverse("home"))
-        return render(request, "users/user_deletion.html", {"form": form})
+    def pre_social_login(self, request, sociallogin):
+        user = sociallogin.account.user
+        if user.id:
+            return
+        try:
+            existing_user = User.objects.get(email=user.email)
+        except ObjectDoesNotExist:
+            pass
+        else:
+            perform_login(
+                request, existing_user, app_settings.EmailVerificationMethod.NONE
+            )
