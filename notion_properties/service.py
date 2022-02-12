6b9b16dc342df51453a1d4f7e58d27361e12cc2d
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2022-02-12 15:33:16 +0100

    now the current tasks schema is always checked when retrieving a recurring task settings

--- notion_properties/service.py
+++ notion_properties/service.py
@@ -29,38 +29,3 @@ def create_properties_dict_for_create_page_api_request_from_property_dto_list(
             property_dto.name
         ] = property_dto.get_notion_property_api_dict_for_create_page_request()
     return create_page_property_dict
-
-
-def is_property_dict_matching_database_schema_dict_list(
-    property_dict, db_schema_property_dict_list
-):
-    if "type" not in property_dict:
-        return False
-    property_type = property_dict["type"]
-    matching_db_schema_property_dict = None
-    for db_property_dict in db_schema_property_dict_list:
-        if (
-            db_property_dict["type"] == property_type
-            and db_property_dict["id"] == property_dict["id"]
-        ):
-            matching_db_schema_property_dict = property_dict
-            break
-    if matching_db_schema_property_dict is None:
-        return False
-    property_value = property_dict["value"]
-    if type in constants.NOTION_SELECT_PROPERTIES:
-        for option in matching_db_schema_property_dict["options"]:
-            if option == property_value:
-                return True
-        return False
-    elif type == "number":
-        try:
-            value_as_number = int(property_value)
-        except ValueError:
-            try:
-                value_as_number = float(property_value)
-                return True
-            except ValueError:
-                return False
-    else:
-        return True
