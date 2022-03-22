07663c18397226ca31a89555690ec9b2ba8ebce6
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2022-03-22 18:41:26 +0100

    increased free tasks number, added notion content template preview to allow for upgrade

--- tasks/tests.py
+++ tasks/tests.py
@@ -447,30 +447,6 @@ class TestCreateRecurringTasks(TasksTestCase):
         self.assertEqual(response.status_code, 302)
         self.assert_task_was_created()
 
-    def test_task_cannot_be_created_because_of_usage_limits(self):
-        RecurringTask.objects.create(
-            interval=RecurringTask.TaskIntervals.EVERY_DAY.value,
-            start_time=DEFAULT_RECURRING_TASK_TEST_STARTIME_DATETIME,
-            owner=self.user,
-            name="12345",
-            database=self.sample_database,
-            properties_json={"property": "12345"},
-        )
-        RecurringTask.objects.create(
-            interval=RecurringTask.TaskIntervals.EVERY_DAY.value,
-            start_time=DEFAULT_RECURRING_TASK_TEST_STARTIME_DATETIME,
-            owner=self.user,
-            name="12345",
-            database=self.sample_database,
-            properties_json={"property": "12345"},
-        )
-        self.client.force_login(
-            get_user_model().objects.get_or_create(username=self.user.username)[0]
-        )
-        response = self.client.post("/create-recurring-task/", self.create_payload)
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(RecurringTask.objects.all().count(), 2)
-
     def test_cannot_create_recurring_task_without_workspace_access(self):
         NotionWorkspaceAccess.objects.all().delete()
         self.client.force_login(
@@ -1191,23 +1167,6 @@ class TestDuplicateRecurringTasksDatabase(TasksTestCase):
         response = self.client.post(self.request_url)
         self.assertEqual(response.status_code, 302)
         self.assert_task_was_duplicated()
-
-    def test_recurring_task_not_duplicated_because_of_rate_limit(self):
-        another_recurring_task = RecurringTask.objects.create(
-            interval=RecurringTask.TaskIntervals.EVERY_DAY.value,
-            start_time=DEFAULT_RECURRING_TASK_TEST_STARTIME_DATETIME,
-            owner=self.user,
-            name=self.default_task_name,
-            database=self.old_database,
-            properties_json={"property": "12345"},
-        )
-        self.client.force_login(
-            get_user_model().objects.get_or_create(username=self.user.username)[0]
-        )
-        response = self.client.post(self.request_url)
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(RecurringTask.objects.count(), 2)
-        self.assertEqual(Schedule.objects.count(), 2)
 
 
 class TestGetRecurringTaskView(TasksTestCase):
